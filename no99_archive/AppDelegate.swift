8fe55bf1ef14d0d08ea766ebc9143ef7c2f350d2
almightyken0425 <almightyken0425@gmail.com>
2026-01-06 08:14:43 +0800

    update

---
+++ no99_archive/AppDelegate.swift
@@ -0,0 +1,85 @@
+import UIKit
+import React
+import React_RCTAppDelegate
+import ReactAppDependencyProvider
+import FirebaseCore
+import FirebaseMessaging
+import UserNotifications
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate, MessagingDelegate {
+  var window: UIWindow?
+  var reactNativeDelegate: ReactNativeDelegate?
+  var reactNativeFactory: RCTReactNativeFactory?
+
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+
+    FirebaseApp.configure()
+    
+    // [START set_messaging_delegate]
+    Messaging.messaging().delegate = self
+    // [END set_messaging_delegate]
+
+    // Register for remote notifications. This shows a permission dialog on first run, to
+    // show the dialog at a more appropriate time move this registration accordingly.
+    // [START register_for_notifications]
+
+    UNUserNotificationCenter.current().delegate = self
+
+//    let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+//    UNUserNotificationCenter.current().requestAuthorization(
+//      options: authOptions,
+//      completionHandler: { _, _ in }
+//    )
+
+    application.registerForRemoteNotifications()
+
+    // [END register_for_notifications]
+
+    // React Native 初始化
+    let delegate = ReactNativeDelegate()
+    let factory = RCTReactNativeFactory(delegate: delegate)
+    delegate.dependencyProvider = RCTAppDependencyProvider()
+
+    reactNativeDelegate = delegate
+    reactNativeFactory = factory
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+    factory.startReactNative(
+      withModuleName: "MeFresh",
+      in: window,
+      launchOptions: launchOptions
+    )
+
+    return true
+  }
+
+  // MARK: - Messaging Delegate
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("🪄 FCM Token: \(fcmToken ?? "nil")")
+  }
+
+  // MARK: - Notification Delegate
+  func userNotificationCenter(_ center: UNUserNotificationCenter,
+                              willPresent notification: UNNotification,
+                              withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    completionHandler([.alert, .badge, .sound])
+  }
+}
+
+class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
+  override func sourceURL(for bridge: RCTBridge) -> URL? {
+    self.bundleURL()
+  }
+
+  override func bundleURL() -> URL? {
+#if DEBUG
+    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+#else
+    Bundle.main.url(forResource: "main", withExtension: "jsbundle")
+#endif
+  }
+}
